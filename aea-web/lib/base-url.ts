3297effe0a0c8a5b28dfcf080f7b322d9c015d22
Gabriel Voss <voss.git@gmail.com>
2025-01-20 01:30:45 +0100

    url

--- aea-web/lib/base-url.ts
+++ aea-web/lib/base-url.ts
@@ -1,5 +1,5 @@
 export default function getBaseURL(){
     if(typeof window !== "undefined") return ""
     if(process.env.VRECEL_URL) { return `https://${process.env.VERCEL_URL}`;}
-    return "http://localhost:3000";
+    return "https://www.aeapolimi.it";
 }
